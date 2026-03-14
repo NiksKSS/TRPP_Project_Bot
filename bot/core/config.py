b12e1d91335e7f0ac766f778ea933a20cec18155
Liza <kuznecovaElizaveta2006@yandex.ru>
2026-03-14 15:33:49 +0300

    base core

---
+++ bot/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Настройки бота."""
+    bot_token: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
