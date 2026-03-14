0ca645697ca14640a949406b8d52941253de9a19
NiksKSS <v.denisenko2307@gmail.com>
2026-03-14 15:10:12 +0300

    ocr

---
+++ ocr.py
@@ -0,0 +1,13 @@
+
+# --- Функция для распознавания текста на изображении (OCR) ---
+import pytesseract
+from PIL import Image
+
+def ocr(image_path, lang: str = "rus+eng"):
+    return pytesseract.image_to_string(Image.open(image_path), lang=lang).strip()
+
+text = ocr("image.png")
+print(text)
+# --- Функция для распознавания текста на изображении ---
+
+
